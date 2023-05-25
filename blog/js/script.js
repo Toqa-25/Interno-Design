7ad1df33939af1c38feb3b4c228341253758971a
Toqa-25 <toqa35abdelsalam757@gmail.com>
2023-05-25 05:40:34 +0300

    adding plog page file

---
+++ blog/js/script.js
@@ -0,0 +1,19 @@
+let bar = document.querySelector(".bar"),
+    menuList = document.querySelector(".menu-list"),
+    search = document.querySelector(".search"),
+    searchInput = document.querySelector(".input-search");
+
+    bar.onclick = () => {
+        console.log("bar")
+        menuList.classList.toggle("show-menu-list")
+        if ( searchInput.classList.contains("show-input-search")){
+            searchInput.classList.remove("show-input-search")
+        }
+    }
+    search.onclick = () => {
+        console.log("search")
+        searchInput.classList.toggle("show-input-search")
+        if ( menuList.classList.contains("show-menu-list")){
+            menuList.classList.remove("show-menu-list")
+        }
+    }
